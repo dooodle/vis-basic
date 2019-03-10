1a8fe62d76f6bd731bec7ca10a23c4facb43b8aa
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-03-10 22:02:29 +0000

    basic scatter off economy

--- cmd/scatter/basicvis.js
+++ cmd/scatter/basicvis.js
@@ -1,24 +1,47 @@
-function createSoccerViz() {
-  d3.csv("mondial/country.csv").then(data => {overallTeamViz(data)}) // v5
-  //d3.csv("worldcup.csv", data => {overallTeamViz(data)}) v4            
-function overallTeamViz(incomingData) {
+function createVis() {
+  d3.csv("mondial/economy.csv").then(data => {overallVis(data)}) // v5         
+function overallVis(incomingData) {
+  var maxInflation = d3.max(incomingData, d => d.inflation)
+  var minInflation = d3.min(incomingData, d => d.inflation)
+  var maxUnemployment = d3.max(incomingData, d => d.unemployment)
+  
+  var yScale = d3.scaleLinear().domain([0,maxUnemployment]).range([460,0])
+  var xScale = d3.scaleLinear().domain([minInflation,maxInflation]).range([20,480]) 
+  
   d3.select("svg")
-    .append("g")                                                         
-    .attr("id", "teamsG")
-    .attr("transform", "translate(50,300)")
     .selectAll("g")
     .data(incomingData)
     .enter()
     .append("g")
-    .attr("class", "overallG")
-    .attr("transform", (d, i) =>"translate(" + (i * 50) + ", 0)")        
-  var teamG = d3.selectAll("g.overallG");                            
-  teamG
+    .attr("class", "overallG")      
+  var countries = d3.selectAll("g.overallG");                            
+  countries
     .append("circle")
-    .attr("r", 20)
-   teamG
+    .attr("r", 1)
+    .attr("cx", d => xScale(d.inflation))
+    .attr("cy", d => yScale(d.unemployment))
+   countries
     .append("text")
-    .attr("y", 30)
-    .text(d => d.country)
+    .attr("x", d => xScale(d.inflation))
+    .attr("y", d => yScale(d.unemployment))
+    //.text(d => d.country)
+    
+     xAxis = d3.axisBottom().scale(xScale).ticks(4)
+ 	 d3.select("svg").append("g").attr("id","xAxis")
+ 	 .attr("transform", "translate(0,480)")
+ 	 .call(xAxis)
+ 	 .append("text")
+ 	 .attr("x","250")
+ 	 .attr("y","10")
+ 	 .text("inflation")
+ 	 
+     yAxis = d3.axisRight().scale(yScale).ticks(4)
+ 	 d3.select("svg").append("g").attr("id","yAxis")
+ 	 .attr("transform", "translate(480,0)") 
+ 	 .call(yAxis)   
+ 	 .append("text")
+ 	 .attr("transform", "rotate(90,-100,110)") 
+ 	 .text("unemployment")	 
     }
 }
+
